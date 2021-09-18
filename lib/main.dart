e6afd829248c8294a9c36290ce45bf4416ada464
ideiasst <contatos@ideiasst.com.br>
2021-09-18 18:22:57 -0300

    Configuração firebase android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:app_filmes/modules/splash/splash_module.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+void main() async {
+  //iniciar o flutter antes de iniciar o app
+  WidgetsFlutterBinding.ensureInitialized();
+  //iniciar o firebase
+  await Firebase.initializeApp();
+  //iniciar o app
   runApp(MyApp());
 }
 
@@ -11,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
